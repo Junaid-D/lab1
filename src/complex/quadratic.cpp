43c81af0683564659e78aa8c278f41d04fb77649
Unknown <1094837@students.wits.ac.za>
2018-07-26 20:24:42 +0200

    put in ability for a=0 in quadratic

--- src/complex/quadratic.cpp
+++ src/complex/quadratic.cpp
@@ -12,7 +12,7 @@ int main()
 	{
 		using cFloat=std::complex<float>;//alias for complex of <float>
 
-		float temp=0;
+		float temp=0;//a single temp for input
 
 		std::cout<<"Enter a"<<std::endl;
 		std::cin>>temp;
@@ -26,16 +26,25 @@ int main()
 		std::cin>>temp;
 		cFloat cComp=cFloat{temp,0};
 
+		//creating complex objs
+		if(aComp.real()!=0.f)
+		{
+			cFloat root = std::sqrt(std::pow(bComp,2)-aComp*cComp*4.f)/(2.f*aComp);
+
+			cFloat ans=(-bComp)/(2.f*aComp);
+			std::cout<<"The roots are :"<<std::endl;
+			std::cout<<ans+root<<std::endl;
+			std::cout<<ans-root<<std::endl;
+		}else
+		{
+			std::cout<<"This is not a quadratic but the root is:"<<std::endl;
+			std::cout<<(-cComp/bComp)<<std::endl;
+		}
 
 
-		cFloat root = std::sqrt(std::pow(bComp,2)-aComp*cComp*4.f)/(2.f*aComp);
-
-		cFloat ans=(-bComp)/(2.f*aComp);
-		std::cout<<"The roots are :"<<std::endl;
-		std::cout<<ans+root<<std::endl;
-		std::cout<<ans-root<<std::endl;
 		std::cout<<"Would you like to do another?"<<std::endl;
-		
+		std::cin.clear(); 
+		std::cin.ignore(INT8_MAX,'\n'); 
 		std::cin>>response;
 
 	}while(response!='q');
